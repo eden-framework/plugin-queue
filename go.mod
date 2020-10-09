3bbfd68dce589f8a7fae977b9c6a3fea2ed7c33c
liyiwen <johnnyeven@gmail.com>
2020-10-09 13:39:34 +0800

    regenerate driver

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.14
 require (
 	github.com/cornelk/hashmap v1.0.1
 	github.com/eden-framework/common v0.0.3
-	github.com/eden-framework/eden-framework v1.0.15
+	github.com/eden-framework/enumeration v1.0.0
 	github.com/eden-framework/plugin-kafka v0.0.7
 	github.com/eden-framework/plugin-redis v0.1.2
 	github.com/eden-framework/plugins v0.0.6
--- queue/driver__generated.go
+++ queue/driver__generated.go
@@ -5,13 +5,13 @@ import (
 	"encoding"
 	"errors"
 
-	github_com_eden_framework_eden_framework_pkg_enumeration "github.com/eden-framework/eden-framework/pkg/enumeration"
+	github_com_eden_framework_enumeration "github.com/eden-framework/enumeration"
 )
 
 var InvalidDriver = errors.New("invalid Driver")
 
 func init() {
-	github_com_eden_framework_eden_framework_pkg_enumeration.RegisterEnums("Driver", map[string]string{
+	github_com_eden_framework_enumeration.RegisterEnums("Driver", map[string]string{
 		"REDIS":   "redis",
 		"KAFKA":   "kafka",
 		"BUILDIN": "buildin",
